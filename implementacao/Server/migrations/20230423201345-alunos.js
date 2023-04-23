80aaecc0b4241b0e89de7653eb5ca5a0e06b993b
Eric Jardim <90854156+ericgcjardim@users.noreply.github.com>
2023-04-23 20:52:55 -0300

    backend

--- implementacao/Server/migrations/20230423201345-alunos.js
+++ implementacao/Server/migrations/20230423201345-alunos.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-    await queryInterface.createTable('produtos', { 
+    await queryInterface.createTable('alunos', { 
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
