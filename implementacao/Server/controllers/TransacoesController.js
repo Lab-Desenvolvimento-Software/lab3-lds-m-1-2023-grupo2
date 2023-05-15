032870615252981993c97040099a58d21d3a1ea6
Eric Jardim <90854156+ericgcjardim@users.noreply.github.com>
2023-05-14 21:36:38 -0300

    commit

--- implementacao/Server/controllers/TransacoesController.js
+++ implementacao/Server/controllers/TransacoesController.js
@@ -3,6 +3,7 @@
 import Transacoes from '../models/Transacoes';
 import professor from './ProfessorController';
 import aluno from './AlunosController'
+import nodemailer from 'nodemailer'
 
 class TransacoesController{
 
@@ -29,6 +30,37 @@ class TransacoesController{
         aluno.adicionarMoedas(req.body.quantidade, req.body.destinatario)
     }
 
+
+//=================email===================//
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"lab3"', // sender address
+      to: "ericjardim007@gmail.com", // list of receivers
+      subject: "Moedas enviadas", // Subject line
+      text: req.body.mensagem, // plain text body
+     // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+//=================email===================//
 
     try {
       await Transacoes.create({
--- implementacao/Server/routes.js
+++ implementacao/Server/routes.js
@@ -18,6 +18,8 @@ router.post('/alunos', alunos.create)
 router.delete('/alunos/:id', alunos.delete)
 router.put('/alunos/:id', alunos.update)
 router.get('/alunos/:id', alunos.show)
+
+
 //empresas
 router.get('/empresas', empresas.index)
 router.post('/empresas', empresas.create)
@@ -27,16 +29,15 @@ router.get('/empresas/:id', empresas.show)
 
 //professores
 
-
-
 //transacoes
 
-router.get('/transacoes/:id', transacoes.index)
-router.post('/transacoes/:id', transacoes.create)
+router.get('/transacoes/:id', transacoes.index) // /transacoes/:id  enviar apenas id pelo parametro, recebe transacoes de entrada e de saida (dois objetos)
+router.post('/transacoes/:id', transacoes.create) // /transacoes/:id
+// post precisa mandar: tipo_usuario ='professor', quantidade, id (usuario e nao do professor/aluno. O mesmo que ta nos parametros do get, esse e o remetente, no caso o professor). destinatario -> o id do aluno que vai receber (se for fazer um dropdown, pega o atributo usuarioId do aluno e envia).
 
 //login
 
-router.post('/login', login.login)
+router.post('/login', login.login)// /login -> envia no body login e senha, salva os dados de retorno na session
 
 router.get('/professorMoedas/:id', professores.showMoedas)
 module.exports = router
