0fb14f8b89ea48744abab706793f296f7d91ef8d
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2023-05-14 22:39:37 -0300

    add post transacoes

--- implementacao/client/src/componentes/Header.js
+++ implementacao/client/src/componentes/Header.js
@@ -4,16 +4,18 @@ import logo from '../imagens/moeda-40x40.png'
 import './Header.css'
 // Libs
 import { Link, Outlet, useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Header = ()=> {
 
     const navigate = useNavigate()
 
+    const [user_type, setUserType] = useState()
+
     useEffect(()=> {
-        let userId = sessionStorage.getItem("id");
-        if(userId){
+        if(sessionStorage.getItem("id")){
             navigate('/alunos')
+            setUserType(sessionStorage.getItem("tipo"))
         }else{
             navigate('/login')
         }
@@ -31,7 +33,7 @@ const Header = ()=> {
                 <div className={"nav-divider"}></div>
                 <Link to={'/empresas'}>Empresas</Link>
                 <div className={"nav-divider"}></div>
-                <Link to={'/extrato'}>Extrato</Link>
+                <Link to={`/extrato/${user_type}`}>Extrato</Link>
             </nav>
         </header>
         <section id={'main'}>
--- implementacao/client/src/paginas/Extrato.js
+++ implementacao/client/src/paginas/Extrato.js
@@ -1,10 +1,16 @@
 // MUI
 import { DataGrid } from '@mui/x-data-grid';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 // Scripts
 import server from '../scripts/config.js'
 // Libs
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { toast } from 'react-toastify'
+// Estilo
+import './Extrato.css'
 
 const Empresas = ()=> {
 
@@ -12,11 +18,15 @@ const Empresas = ()=> {
         getExtrato()
     },[])
 
+    let { tipo } = useParams()
+
     const [extrato, setExtrato] = useState([])
+
+    let form = useRef(null)
 
     const getExtrato = async()=> {
         try{
-            const res = await axios.get(`${server}/transacoes`)
+            const res = await axios.get(`${server}/transacoes/${sessionStorage.getItem("id")}`)
             // setEmpresas(res)
             console.log(res)
         }catch(erro){
@@ -26,14 +36,49 @@ const Empresas = ()=> {
 
     const columns = [
         // qnt destinatario
-        { field: '', headerName: 'ID Remetente', width: 150 },
-        { field: 'nome', headerName: 'ID Destinatário', width: 150 },
-        { field: 'cnpj', headerName: 'Quantia', width: 150 }
+        { field: 'id', headerName: 'ID Transação', width: 150 },
+        { field: 'cnpj', headerName: 'Quantia', width: 150 },
+        { field: 'id_remetente', headerName: 'ID Remetente', width: 150 },
+        { field: 'id_destinatario', headerName: 'ID Destinatário', width: 150 }
     ]
+
+    const postTransacao = async(e)=> {
+        e.preventDefault()
+        try{
+            let data = new FormData(form.current)
+            data.append("tipo_usuario", `${tipo}`);
+
+            const res = await axios.post(`${server}/transacoes/${sessionStorage.getItem("id")}`, data, {headers: {'Content-Type': 'application/json'}})
+            
+            toast.success('Transação realizada com sucesso.', {toastId: 'sucesso'})
+            console.log(res)
+        }catch(erro){
+            console.log(erro)
+            toast.error('Erro durante a transação.', {toastId: 'falha'})
+        }
+    }
 
     return(
         <>
         <h2>Extrato</h2>
+
+        {
+            tipo === 'professor' &&
+
+            <form id={'transacao'} ref={form} onSubmit={postTransacao}>
+
+                <h3>Nova transação</h3>
+
+            <TextField size={'small'} required fullWidth label="Quantidade" variant="outlined" name={'quantidade'} type={'number'}/>
+            <TextField size={'small'} required fullWidth label="Destinatario" variant="outlined" name={'destinatario'} type={'number'}/>
+            <TextField size={'small'} required fullWidth label="Mensagem" variant="outlined" name={'mensagem'}/>
+
+            <Button type={'submit'} fullWidth variant={'contained'}>Entrar</Button>
+
+            </form>
+        }
+
+
 
         <DataGrid
             rows={extrato}
