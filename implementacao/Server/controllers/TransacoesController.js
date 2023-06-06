fff8b410923a98f9ee54d8312dbb6cb1b550dee1
Eric Jardim <90854156+ericgcjardim@users.noreply.github.com>
2023-06-06 18:00:15 -0300

    PDF

--- implementacao/Server/controllers/TransacoesController.js
+++ implementacao/Server/controllers/TransacoesController.js
@@ -10,7 +10,19 @@ import Usuarios from '../models/Usuarios';
 import vantagens from './VantagensController';
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
+import { Readable } from 'stream';
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
+function formatTransactionForTable(transaction) {
+  const usuarioNome = transaction.destinatario ? transaction.destinatario.NOME : transaction.remetente ? transaction.remetente.NOME : 'N/A';
+  return [
+    transaction.id,
+    transaction.QUANTIDADE,
+    new Date(transaction.createdAt).toLocaleString(),
+    usuarioNome
+  ];
+}
+
 
 class TransacoesController{
 
@@ -38,72 +50,121 @@ class TransacoesController{
     }
 
   async extratoPdf(req,res){
-    // try {
-    //   const transacoesSaida = await Transacoes.findAll({
-    //     where: {
-    //       remetenteId: req.params.id,
-    //     },
-    //   });
-    //   const transacoesEntrada = await Transacoes.findAll({
-    //     where: {
-    //       destinatarioId: req.params.id,
-    //     },
-    //   });
+    try {
+      const transacoesSaida = await Transacoes.findAll({
+        where: {
+          remetenteId: req.params.id,
+        },
+        attributes: ['id', 'QUANTIDADE', 'createdAt'],
+        include: [{
+          model: Usuarios,
+          as: 'destinatario', 
+          attributes: ['NOME'], 
+        }]
+      });
+      
+      const transacoesEntrada = await Transacoes.findAll({
+        where: {
+          destinatarioId: req.params.id,
+        },
+        attributes: ['id', 'QUANTIDADE', 'createdAt'],
+        include: [{
+          model: Usuarios,
+          as: 'remetente', 
+          attributes: ['NOME'], 
+        }]
+      });
+      
+      // Preparar estrutura do documento
+      const docDefinition = {
+        content: [
+          { text: 'Relatório de Extrato', style: 'header' },
+          { text: 'Transações de Saída', style: 'subheader' },
+          {
+            table: {
+              headerRows: 1,
+              widths: ['*', '*', '*', '*'],
+              body: [
+                [
+                  { text: '#', color: 'white', fillColor: 'red' }, 
+                  { text: 'Quantidade', color: 'white', fillColor: 'red' },
+                  { text: 'Data e Hora', color: 'white', fillColor: 'red' }, 
+                  { text: 'Destinatário', color: 'white', fillColor: 'red' },
+                ],
+                ...transacoesSaida.map(formatTransactionForTable)
+              ],
+            },
+          },
+          { text: 'Transações de entrada', style: 'subheader' },
+          {
+            table: {
+              headerRows: 1,
+              widths: ['*', '*', '*', '*'],
+              body: [
+                [
+                  { text: '#', color: 'white', fillColor: 'green' }, 
+                  { text: 'Quantidade', color: 'white', fillColor: 'green' }, 
+                  { text: 'Data e Hora', color: 'white', fillColor: 'green' }, 
+                  { text: 'Remetente', color: 'white', fillColor: 'green' },
+                ],
+                ...transacoesEntrada.map(formatTransactionForTable)
+              ],
+            },
+          },
+        ],
+        styles: {
+          header: {
+            fontSize: 18,
+            bold: true,
+            margin: [0, 0, 0, 10],
+          },
+          subheader: {
+            fontSize: 16,
+            bold: true,
+            margin: [0, 10, 0, 5],
+          },
+        },
+      };
 
-    //   // Preparar estrutura do documento
-    //   const docDefinition = {
-    //     content: [
-    //       { text: 'Relatório de Extrato', style: 'header' },
-    //       { text: 'Transações de Saída', style: 'subheader' },
-    //       ...transacoesSaida.map((transaction) =>
-    //         JSON.stringify(transaction)
-    //       ), 
-    //       { text: 'Transações de entrada', style: 'subheader' },
-    //       ...transacoesEntrada.map((transaction) =>
-    //         JSON.stringify(transaction)
-    //       ), // JSON.stringify por outra funcao de formatação
-    //     ],
-    //     styles: {
-    //       header: {
-    //         fontSize: 18,
-    //         bold: true,
-    //         margin: [0, 0, 0, 10],
-    //       },
-    //       subheader: {
-    //         fontSize: 16,
-    //         bold: true,
-    //         margin: [0, 10, 0, 5],
-    //       },
-        
-    //     },
-    //   };
-
-    //   const pdfDoc = pdfMake.createPdf(docDefinition);
-    //   pdfDoc.getBuffer((buffer) => {
-    //     res.setHeader('Content-Disposition', 'attachment;filename=extrato.pdf'); //browser faz o download do arquivo
-    //     res.setHeader('Content-Type', 'application/pdf');
-    //     res.send(buffer);
-    //   });
-    // } catch (error) {
-    //   console.log(error);
-    //   return res.status(500).json({ error });
-    // }
-    const docDefinition = {
-      content: [
-        'This is a test PDF',
-      ],
-    };
-    
-    const pdfDoc = pdfMake.createPdf(docDefinition);
+      const pdfDoc = pdfMake.createPdf(docDefinition);
     pdfDoc.getBuffer((buffer) => {
-      res.setHeader('Content-Disposition', 'attachment;filename=extrato.pdf'); 
+      const stream = new Readable();
+      stream.push(buffer);
+      stream.push(null);
+      
+      res.setHeader('Content-Disposition', 'attachment;filename=extrato.pdf');
       res.setHeader('Content-Type', 'application/pdf');
-      res.send(buffer);
+      stream.pipe(res);
     });
-  }
-    
+
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
+
+    }
+
   async create(req, res) {
     // front tem que enviar um tipo de usuario
+
+
+    try {
+      console.log('teste')
+      await Transacoes.create({
+        QUANTIDADE: req.body.quantidade,
+
+        remetenteId: req.params.id,
+
+        destinatarioId: req.body.destinatario
+      }
+    );
+
+
+
+      return res.status(200).json("transacao realizada com sucesso!");
+    } catch (error) {
+      res.status(500).json({ error });
+    }
 
     if(req.body.tipo_usuario=='professor'){
         professor.subtrairMoedas(req.body.quantidade, req.params.id )
@@ -157,22 +218,7 @@ class TransacoesController{
       }
     }
 
-    try {
-      await Transacoes.create({
-        QUANTIDADE: req.body.quantidade,
-
-        remetenteId: req.params.id,
-
-        destinatarioId: req.body.destinatario
-      }
-    );
-
-
-
-      return res.status(200).json("transacao realizada com sucesso!");
-    } catch (error) {
-      res.status(500).json({ error });
-    }
+ 
   }
 
 }
